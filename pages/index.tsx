b71ad49942a103e5204812f248806037ed19f1a9
Ganessh Kumar <rpganesshkumar@gmail.com>
2022-01-25 20:17:10 +0000

    adding server side rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,27 @@
 import Head from 'next/head'
 import React from 'react';
-import styles from '../styles/Home.module.css'
 
-import { wordlist } from '../data/wordlist'
+import { getWords } from '../data/wordlist'
 
 const getIndexForDay = (dayOffset: number = 0) => {
   const msInADay = 24 * 60 * 60 * 1000
   const targetDay = Date.now() + (dayOffset * msInADay)
   const firstDay = new Date("2021-06-19 00:00:00").getTime()
-  return Math.floor((targetDay - firstDay) / msInADay) % wordlist.length
+  return Math.floor((targetDay - firstDay) / msInADay)
 }
 
-const Home = (): JSX.Element => {
+const Home = ({words}) => {
+
+  const todayIndex = getIndexForDay(0)
+  const todayAnswer = words[todayIndex]
+  const tomorrowAnswer = words[todayIndex + 1]
+  console.log('words', words)
+  console.log(todayIndex)
+
   return (
     <div>
       <Head>
-        <title>{wordlist[getIndexForDay(0)]} | Wordle Answer</title>
+        <title>{todayAnswer} | Wordle Answer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -28,17 +34,27 @@ const Home = (): JSX.Element => {
         <div>
           <div className="flex flex-col justify-center items-center mt-20 mb-20">
             <div>Today's answer</div>
-            <div className="text-5xl wordle-success">{wordlist[getIndexForDay(0)]}</div>
+            <div className="text-5xl wordle-success">{todayAnswer}</div>
           </div>
           <div className="flex flex-col justify-center items-center">
             <div>Tomorrow's answer</div>
-            <div className="text-3xl wordle-warning">{wordlist[getIndexForDay(1)]}</div>
+            <div className="text-3xl wordle-warning">{tomorrowAnswer}</div>
           </div>
         </div>
         <div></div>
       </div>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const todayIdx = getIndexForDay(0);
+  const indices = [todayIdx - 1, todayIdx, todayIdx + 1, todayIdx + 2]
+  const words = {}
+  getWords(indices)
+    .forEach((value, idx) => words[indices[idx]] = value)
+
+  return { props: {  words  } }
 }
 
 export default Home;
